58ac5aa2dbe278eddb61585ba8b36223b41d9be9
Aman Sharma <amanjdj@gmail.com>
2025-12-23 13:08:09 +0530

    Add user details page with API integration and stae handling. Tested.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
+import UserDetails from "./pages/UserDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/users/:id" element={<UserDetails />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/UserDetails.jsx
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { fetchUserBYID } from "../services/userApi";
+import Loader from "../components/Loader";
+import ErrorState from "../components/ErrorState";
+
+function UserDetails() {
+  const { id } = useParams();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const data = await fetchUserBYID(id);
+        setUser(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadUser();
+  }, [id]);
+
+  if (loading) return <Loader />;
+  if (error) return <ErrorState message={error} />;
+
+  return (
+    <div className="p-6 max-w-xl mx-auto">
+      <Link to="/" className="text-blue-600 text-sm mb-4 inline-block">
+        ⬅️ Back to Dashboard
+      </Link>
+      <h2 className="text-2xl font-semibold mb-4">User Details</h2>
+
+      <div className="space-y-2 bg-white p-4 border rounded-lg">
+        <p>
+          <strong>Name:</strong> {user.name}
+        </p>
+        <p>
+          <strong>Email:</strong> {user.email}
+        </p>
+        <p>
+          <strong>Phone:</strong> {user.phone}
+        </p>
+        <p>
+          <strong>Website:</strong> {user.website}
+        </p>
+        <p>
+          <strong>Company:</strong> {user.company.name}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default UserDetails;
